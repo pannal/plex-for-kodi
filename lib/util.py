a46ced5cc2a299af4172172d2f3b236d5a919144
panni <1359593+pannal@users.noreply.github.com>
2025-09-07 03:55:19 +0200

    Core/CoreELEC/LG WebOS: Only default to Alternate seek if <superviseaudiodelay> is not set in advancedsettings.xml (thanks @MikeTeavee)

--- lib/util.py
+++ lib/util.py
@@ -37,6 +37,7 @@ from .kodi_util import (ADDON, xbmc, xbmcvfs, xbmcaddon, xbmcgui, translatePath,
 from .properties import setGlobalProperty, setGlobalBoolProperty, waitForGPEmpty, waitForConsumption, getGlobalProperty
 # noinspection PyUnresolvedReferences
 from .addonsettings import addonSettings, AddonSettings
+from .advancedsettings import adv
 from .settings_util import getSetting, getUserSetting, setSetting, USER_SETTINGS, JSON_SETTINGS, DEFAULT_SETTINGS
 from .monitor import MONITOR
 
@@ -702,7 +703,7 @@ def getPlatformFlavor():
 
 
 platformFlavor = getPlatformFlavor()
-altSeekRecommended = platformFlavor != 'default'
+altSeekRecommended = platformFlavor in ("CoreELEC", "LG WebOS") and (KODI_VERSION_MAJOR >= 21 and not adv.has_audio_fix)
 
 
 def getRunningAddons():
